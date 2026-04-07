b008d505f1c3c6c65283d8add37f2a9a85b3bcb8
moustafafazeleabas-cpu <moustafafazeleabas@gmail.com>
2026-04-07 18:57:26 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-voila : import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { createClient } from '@supabase/supabase-js';
 
 // --- 🔒 SÉCURITÉ : VARIABLES D'ENVIRONNEMENT ---
